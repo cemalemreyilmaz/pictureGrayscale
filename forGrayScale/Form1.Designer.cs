5397448f7770dd602f470651294c5e3e0c65a70e
YILMAZ\Emre YILMAZ <cemalemreyilmaz@gmail.com>
2017-05-26 20:30:33 +0300

    001gs

--- forGrayScale/Form1.Designer.cs
+++ forGrayScale/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGozat = new System.Windows.Forms.Button();
+            this.btnGrayScale = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGozat
+            // 
+            this.btnGozat.Location = new System.Drawing.Point(149, 284);
+            this.btnGozat.Name = "btnGozat";
+            this.btnGozat.Size = new System.Drawing.Size(149, 23);
+            this.btnGozat.TabIndex = 0;
+            this.btnGozat.Text = "Select Photo";
+            this.btnGozat.UseVisualStyleBackColor = true;
+            this.btnGozat.Click += new System.EventHandler(this.btnGozat_Click);
+            // 
+            // btnGrayScale
+            // 
+            this.btnGrayScale.Location = new System.Drawing.Point(149, 313);
+            this.btnGrayScale.Name = "btnGrayScale";
+            this.btnGrayScale.Size = new System.Drawing.Size(149, 23);
+            this.btnGrayScale.TabIndex = 1;
+            this.btnGrayScale.Text = "GrayScale";
+            this.btnGrayScale.UseVisualStyleBackColor = true;
+            this.btnGrayScale.Click += new System.EventHandler(this.btnGrayScale_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 235);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(149, 342);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(149, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 374);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGrayScale);
+            this.Controls.Add(this.btnGozat);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGozat;
+        private System.Windows.Forms.Button btnGrayScale;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
